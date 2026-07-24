0de01960fa2bae3aa3326fa0dd6738b0dec26472
vigneshk.2105 <vigneshkumar20105@gmail.com>
2026-07-24 17:32:33 +0530

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Persistence;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ActivitiesController(AppDbContext context) : BaseApiController
+{
+    [HttpGet]
+
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+        if (activity == null) return NotFound();
+
+        return activity;
+    }
+}
---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
